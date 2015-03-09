1a103ebb7ede38afea99cca1f30e3a00fafb4650
mataddy <mataddy@outlook.com>
2015-03-09 16:13:12 -0500

    done with yelp eg

---
+++ code/yelp_linmod.R
@@ -0,0 +1,21 @@
+library(Matrix)
+
+yelp <- read.table("data/yelp_phrases.txt",sep="|",comment="",quote=NULL)
+
+x <- sparseMatrix( 
+			i=yelp[,1]+1, j=as.numeric(yelp[,2]), x=rep(1,nrow(yelp)),
+			dimnames=list(NULL, levels(yelp[,2])) )
+x <- x[,colSums(x>0)>5]
+y <- as.numeric(sparseMatrix( i=yelp[,1]+1, j=yelp[,3]+1 )[,2])
+testset <- read.table("data/yelp_test.txt")[,1] + 1
+n <- nrow(dtm)
+
+library(gamlr)
+logistic = gamlr(x[-testset,], y[-testset], lmr=1e-4, family="binomial", verb=TRUE)
+
+png(file="graphs/yelp_logistic.png", width=12,height=5, units="in", res=360)
+plot(logistic)
+dev.off()
+
+yhat <- as.numeric(predict(logistic, x[testset,], type="response") > 0.5)
+mean(y[testset]==yhat)
