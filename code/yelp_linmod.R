145c385585c8c81cae9cbf6824a2e2bc0b0f6e3b
mataddy <mataddy@outlook.com>
2015-03-11 11:39:32 -0500

    added indep sentences and run from the top

--- code/yelp_linmod.R
+++ code/yelp_linmod.R
@@ -10,11 +10,12 @@ y <- as.numeric(sparseMatrix( i=yelp[,1]+1, j=yelp[,3]+1 )[,2])
 testset <- read.table("data/yelp_test.txt")[,1] + 1
 
 library(gamlr)
-logistic = gamlr(x[-testset,], y[-testset], lmr=1e-4, family="binomial", verb=TRUE)
+logistic = gamlr(x[-testset,], y[-testset], lmr=1e-4, family="binomial")
 
 png(file="graphs/yelp_logistic.png", width=12,height=5, units="in", res=360)
 plot(logistic)
 dev.off()
 
 yhat <- as.numeric(predict(logistic, x[testset,], type="response") > 0.5)
-cat("MC rate", mean(y[testset]==yhat), "\n")
+cat("MC rate; bad: ", 1-mean(y[testset][1:10000]==yhat[1:10000]), 
+	" good: ", 1-mean(y[testset][10001:20000]==yhat[10001:20000]), "\n")
