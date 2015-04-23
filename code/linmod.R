0641a17fdb39e6e304a5495bf666ef3ff3a0f1f5
mataddy <mataddy@outlook.com>
2015-04-23 18:28:17 -0500

    new full run

--- code/linmod.R
+++ code/linmod.R
@@ -1,5 +1,5 @@
-library(textir)
-library(data.table)
+suppressMessages(library(textir))
+suppressMessages(library(data.table))
 
 ## read in the text
 revs <- read.table("data/yelp_phrases.txt",
@@ -14,45 +14,15 @@ x <- x[-emptyrev,colSums(x>0)>5]
 
 stars <- tapply(revs$stars, revs$id, mean)
 samp <- tapply( revs$sample=="test", revs$id, mean)
+test <- which(samp==1)
 
-### COARSE logit word-count prediction
-coarse <- which( stars %in% c(1,2,5) )
-xcoarse <- x[coarse, ]
-xcoarse <- xcoarse[, colSums(xcoarse>0)>5]
-ycoarse <- as.numeric(stars[coarse]>2)
-testcoarse <- which(samp[coarse]==1)
-
-coarsefit <- function(x, y, testset, ...){
-    fit = gamlr(x[-testset,], y[-testset], ...)
-    phat <- predict(fit, x[testset,], type="response")
-    yhat <- as.numeric(phat > .5)
-    cat("\nMCR bad: ", mean(yhat[y[testset]==0]), 
-	   ", good: ", 1-mean(yhat[y[testset]==1]), 
-	   ", overall: ", mean(y[testset]!=yhat))
-    dev <- sum(-2*log(phat[y[testset]==1])) + sum(-2*log(1-phat[y[testset]==0]))
-    cat("  DEV: ", dev/length(testset), "\n")
-    return(fit)
-}
-
-logitcoarse <- coarsefit(xcoarse, ycoarse, testcoarse, 
-    verb=TRUE, family="binomial", lambda.min.ratio=1e-4)
-# MCR bad:  0.07883443 , good:  0.02898715 , overall:  0.04567453  DEV:  0.2674133
-
-png(file="graphs/yelp_logistic.png", width=12,height=5, units="in", res=360)
-plot(logitcoarse)
-invisible(dev.off())
-
-nosd <- coarsefit(xcoarse, ycoarse, testcoarse, 
-    verb=TRUE, family="binomial", lambda.min.ratio=1e-2, standardize=FALSE)
-# MCR bad:  0.2990739 , good:  0.05877004 , overall:  0.1392166  DEV:  0.6760135
-
-## D2V stuff
-dv0train <- fread("data/yelpD2Vtrain0.csv")
-dv0test <- fread("data/yelpD2Vtest0.csv")
-dv1train <- fread("data/yelpD2Vtrain1.csv")
-dv1test <- fread("data/yelpD2Vtest1.csv")
-all(dv0test[,id]==dv1test[,id])
-all(dv0test[,stars]==dv1test[,stars])
+## read d2v
+dv0train <- fread("data/yelpD2Vtrain0.csv", verbose=FALSE)
+dv0test <- fread("data/yelpD2Vtest0.csv", verbose=FALSE)
+dv1train <- fread("data/yelpD2Vtrain1.csv", verbose=FALSE)
+dv1test <- fread("data/yelpD2Vtest1.csv", verbose=FALSE)
+# all(dv0test[,id]==dv1test[,id])
+# all(dv0test[,stars]==dv1test[,stars])
 vecvar <- paste("x",1:100,sep="")
 dv0x <- rbind(as.matrix(dv0train[,vecvar,with=FALSE]),
             as.matrix(dv0test[,vecvar,with=FALSE]))
@@ -60,112 +30,199 @@ dv1x <- rbind(as.matrix(dv1train[,vecvar,with=FALSE]),
             as.matrix(dv1test[,vecvar,with=FALSE]))
 dvx <- cbind(dv0x,dv1x)
 dvstars <- c(dv0train[,stars], dv0test[,stars])
-dvsamp <- c(rep(FALSE, nrow(dv0train)),rep(TRUE, nrow(dv0test)))
+dvtest <- nrow(dv0train)+1:nrow(dv0test)
 
-# create coarse dv designs
-dvcoarse <- which(dvstars %in% c(1,2,5))
-dv0xcoarse <- dv0x[dvcoarse, ]
-dv1xcoarse <- dv1x[dvcoarse, ]
-dvxcoarse <- dvx[dvcoarse, ]
-dvycoarse <- as.numeric(dvstars[dvcoarse]>2)
-dvtestcoarse <- which(dvsamp[dvcoarse])
-
-## all run at zero lambda; AICc selects most complex model anyways
-dv0fit <- coarsefit(dv0xcoarse, dvycoarse, dvtestcoarse,
-    verb=TRUE, family="binomial", lambda.start=0)
-# MCR bad:  0.2118103 , good:  0.02004377 , overall:  0.08441111  DEV:  0.4359189 
-dv1fit <- coarsefit(dv1xcoarse, dvycoarse, dvtestcoarse,
-    verb=TRUE, family="binomial", lambda.start=0)
-# MCR bad:  0.5102599 , good:  0.01866144 , overall:  0.1836688  DEV:  0.7584851 
-dvfit <- coarsefit(dvxcoarse, dvycoarse, dvtestcoarse,
-    verb=TRUE, family="binomial", lambda.start=0)
-# MCR bad:  0.2026904 , good:  0.02061974 , overall:  0.08173261  DEV:  0.4349279 
-
-### mnir
 library(parallel)
-cl <- makeCluster(4, type="FORK")
+cl <- makeCluster(6, type="FORK")
 
-fitir <- mnlm(cl=cl, ycoarse[-testcoarse], xcoarse[-testcoarse,], verb=1, bins=2)
-z <- srproj(fitir, xcoarse, select=100)
-fwd <- gamlr(z[-testcoarse,], ycoarse[-testcoarse], family="binomial", lambda.start=0)
-phatir <- predict(fwd, z[testcoarse,], type="response")
-yhatir <- as.numeric(  phatir > 0.5 )
-dev <- sum(-2*log(phatir[ycoarse[testcoarse]==1])) + 
-            sum(-2*log(1-phatir[ycoarse[testcoarse]==0]))
-cat("\nMCR bad: ", mean(yhatir[ycoarse[testcoarse]==0]), 
-       ", good: ", 1-mean(yhatir[ycoarse[testcoarse]==1]), 
-       ", overall: ", mean(ycoarse[testcoarse]!=yhatir),
-        "  DEV: ", dev/length(testcoarse), "\n")
-# MCR bad:  0.08018974 , good:  0.05479141 , overall:  0.06329401   DEV:  0.3439997
+geterr <- function(phat, y, PY=FALSE){
+    if(ncol(phat)==1) phat <- cbind(1-phat,phat)
+    y <- factor(y)
+    yhat <- factor(levels(y)[apply(phat,1,which.max)])
+    cat("mcr ")
+    for(l in levels(y))
+        cat(l, ":", round(
+            mean(yhat[y==l] != y[y==l]),3), ", ", sep="")
+    overall <- mean(yhat !=y)
+    diff <- mean( abs(as.numeric(yhat) - as.numeric(y)) )
+    py <- phat[cbind(1:nrow(phat),y)]
+    lp <- log(py)
+    lp[lp < (-50)] <- -50
+    dev <- mean(-2*lp)
+    cat("\noverall:", round(overall,3), "diff:", round(diff,3), "deviance:", dev, "\n")
+    if(PY) return(py)
+    invisible()
+} 
 
-### FINE logit word-count prediction
-
-testset <- which(samp==1)
-y <- factor(stars)
-
-finefit <- function(x, y, testset, ...){
-    fit = dmr(cl=cl, x[-testset,], y[-testset], ...)
-    phat <- predict(fit, x[testset,], type="response")
-    yhat <- apply(phat,1,which.max)
-
-    cat("\nMCR ")
-    for(k in  1:5)
-        cat(k, ":", mean(yhat[y[testset]==k]!=k), ", ")
-    cat("\noverall: ", mean(y[testset]!=yhat), ".  ")
-
-    pofy <- phat[cbind(1:length(testset),y[testset])]
-    dev <- sum(-2*log(pofy))/length(testset)
-    cat("DEVIANCE =",dev, "\n")
- 
-
-    return(fit)
+getpy <- function(fit, xx, y, testset, PY=FALSE){
+    if(inherits(fit,"randomForest"))
+        phat <- as.matrix(predict(fit, xx[testset,], type="prob"))
+    else 
+        phat <- predict(fit, xx[testset,], type="response")
+    py <- geterr(phat, y[testset], PY=PY)
+    if(PY) return(py) 
+    invisible()
 }
 
-logitfine <- finefit(x, y, testset, lambda.min.ratio=1e-4)
-logitcoarse <- finefit(csx, y, testset,  lambda.min.ratio=1e-4)
-# MCR 1 : 0.4802521 ,2 : 0.753786 ,3 : 0.7738764 ,4 : 0.3591457 ,5 : 0.2329203 , 
-# overall:  0.409976 .  DEVIANCE = 2.023658
-finenosd <- finefit(x, y, testset, lambda.min.ratio=1e-4, standardize=FALSE)
-# MCR 1 : 0.4327731 ,2 : 0.7132389 ,3 : 0.7426264 ,4 : 0.3594363 ,5 : 0.2318972 , 
-# overall:  0.3972555 .  DEVIANCE = 1.9518 
+## define y
+ycoarse <- as.numeric(stars>2)
+ynnp <- cut(stars, c(0,2,3,5))
+yfine <- factor(stars)
+dvycoarse <- as.numeric(dvstars==5)
+dvynnp <- cut(dvstars, c(0,2,3,5))
+dvyfine <- factor(dvstars)
 
-dvtest <- which(dvsamp)
-dvy <- factor(dvstars)
+### W2V inversion
+cat("\n**** W2V INVERSION ****\n")
+w2vprob <- fread("data/fineyelpscore.csv", header=TRUE, verbose=FALSE)
+w2vprob <- as.matrix(w2vprob[-emptyrev,])
 
+nullprob <- as.numeric(table(stars[-test])/length(stars[-test]))
+n <- nrow(w2vprob)
+
+cat("** COARSE **\n")
+w2vpcoarse <- cbind(rowSums(w2vprob[,1:2]),rowSums(w2vprob[,3:5]))
+geterr(w2vpcoarse[test,], ycoarse[test])
+
+cat("** NNP **\n")
+w2vpnnp <- cbind(rowSums(w2vprob[,1:2]),
+    rowSums(w2vprob[,3,drop=FALSE]),
+    rowSums(w2vprob[,4:5,drop=FALSE]))
+geterr(w2vpnnp[test,], ynnp[test])
+
+cat("** FINE **\n")
+geterr(w2vprob[test,], yfine[test])
+
+### COARSE logit word-count prediction
+cat("\n*** COUNTREG ***\n")
+
+cat("** COARSE **\n")
+logitcoarse <- gamlr(x[-test,], ycoarse[-test], 
+                family="binomial")
+pycoarse <- getpy(logitcoarse, x, ycoarse, test, PY=TRUE)
+
+png(file="graphs/yelp_logistic.png", width=12,height=5, units="in", res=360)
+plot(logitcoarse)
+invisible(dev.off())
+
+cat("** NNP **\n")
+logitnnp <- dmr(cl=cl, x[-test,], ynnp[-test])
+pynnp <- getpy(logitnnp, x, ynnp, test, PY=TRUE)
+
+cat("** FINE **\n")
+logitfine <- dmr(cl=cl, x[-test,], yfine[-test])
+pyfine <- getpy(logitfine, x, yfine, test, PY=TRUE)
+
+cat("\n*** W2V and COUNTREG NNP ***\n")
+wx <- cBind(w2vprob,x)
+combof <- dmr(cl,wx[-test,], ynnp[-test])
+getpy(combof, wx, ynnp, test)
+
+## D2V stuff
 ## all run at zero lambda; AICc selects most complex model anyways
-dv0fit <- finefit(dv0x, dvy, dvtest, lambda.min.ratio=1e-4)
-# MCR 1 : 0.7687075 ,2 : 0.9587021 ,3 : 0.9680738 ,4 : 0.1788869 ,5 : 0.4119341 
-# overall:  0.4973102 .  DEVIANCE = 2.312074
-dv1fit <- finefit(dv1x, dvy, dvtest, lambda.min.ratio=1e-4)
-# MCR 1 : 0.9277211 ,2 : 0.9936087 ,3 : 0.9950337 ,4 : 0.1890459 ,5 : 0.4924548 , 
-# overall:  0.5541494 .  DEVIANCE = 2.557209 
-dvfit <- finefit(dvx, dvy, dvtest, lambda.min.ratio=1e-4)
-# MCR 1 : 0.8001701 ,2 : 0.9646018 ,3 : 0.9702022 ,4 : 0.1682862 ,5 : 0.4316323 
-# overall:  0.5057324 .  DEVIANCE = 2.327374 
+cat("\n*** D2V ***\n")
 
-ymat <- sparse.model.matrix(~stars + y-1)
-fitirfine <- mnlm(cl=cl, ymat[-testset,], x[-testset,], verb=1, bins=2)
-zfine <- srproj(fitirfine, x, select=100)
-fwdfine <- dmr(cl, zfine[-testset,], y[-testset], lambda.min.ratio=1e-4)
-pirfine <- predict(fwdfine, zfine[testset,], type="response")
-yirfine <- apply(pirfine,1,which.max)
+cat("** COARSE\n")
+cat("dm0 **\n")
+dv0coarse <- gamlr(dv0x[-dvtest,], dvycoarse[-dvtest],
+                family="binomial", lambda.start=0)
+getpy(dv0coarse, dv0x, dvycoarse, dvtest)
+cat("dm1 **\n")
+dv1coarse <- gamlr(dv1x[-dvtest,], dvycoarse[-dvtest],
+                family="binomial", lambda.start=0)
+getpy(dv1coarse, dv1x, dvycoarse, dvtest)
+cat("dm both **\n")
+dvcoarse <- gamlr(dvx[-dvtest,], dvycoarse[-dvtest],
+                family="binomial", lambda.start=0)
+getpy(dvcoarse, dvx, dvycoarse, dvtest)
 
-cat("\nMCR ")
-for(k in  1:5)
-   print(mean(yirfine[y[testset]==k]!=k))
-cat("\noverall: ", mean(y[testset]!=yirfine), ".  ")
-pofyfine <- pirfine[cbind(1:length(testset),y[testset])]
-devfine <- sum(-2*log(pofyfine))/length(testset)
-cat("DEVIANCE =",devfine, "\n")
-# MCR 
-# [1] 0.6105042
-# [1] 0.9706888
-# [1] 0.9827949
-# [1] 0.2949295
-# [1] 0.3124929
-# overall:  0.4799826. DEVIANCE = 2.298992 
+cat("** NNP\n")
+cat("dm0 **\n")
+dv0nnp <- gamlr(dv0x[-dvtest,], dvynnp[-dvtest],
+                family="binomial", lambda.start=0)
+getpy(dv0nnp, dv0x, dvynnp, dvtest)
+cat("dm1 **\n")
+dv1nnp <- gamlr(dv1x[-dvtest,], dvynnp[-dvtest],
+                family="binomial", lambda.start=0)
+getpy(dv1nnp, dv1x, dvynnp, dvtest)
+cat("dm both **\n")
+dvnnp <- gamlr(dvx[-dvtest,], dvynnp[-dvtest],
+                family="binomial", lambda.start=0)
+getpy(dvnnp, dvx, dvynnp, dvtest)
+
+cat("** FINE\n")
+cat("dm0 **\n")
+dv0fine <- gamlr(dv0x[-dvtest,], dvyfine[-dvtest],
+                family="binomial", lambda.start=0)
+getpy(dv0fine, dv0x, dvyfine, dvtest)
+cat("dm1 **\n")
+dv1fine <- gamlr(dv1x[-dvtest,], dvyfine[-dvtest],
+                family="binomial", lambda.start=0)
+getpy(dv1fine, dv1x, dvyfine, dvtest)
+cat("dm both **\n")
+dvfine <- gamlr(dvx[-dvtest,], dvyfine[-dvtest],
+                family="binomial", lambda.start=0)
+getpy(dvfine, dvx, dvyfine, dvtest)
+
+# mnir
+cat("\n*** MNIR ***\n")
+vmat <- sparse.model.matrix(~stars + yfine-1)
+mnir <- mnlm(cl=cl, vmat[-test,], x[-test,], verb=1, bins=5)
+zir <- srproj(mnir, x, select=100)
+
+cat("** COARSE **\n")
+fwdcoarse <- dmr(cl, zir[-test,], ycoarse[-test], lambda.min.ratio=1e-4)
+getpy(fwdcoarse, zf, ycoarse, test)
+
+cat("** NNP **\n")
+fwdnnp <- dmr(cl, zir[-test,], ynnp[-test], lambda.min.ratio=1e-4)
+getpy(fwdnnp, zf, ynnp, test)
+
+cat("** FINE **\n")
+fwdfine <- dmr(cl, zir[-test,], yfine[-test], lambda.min.ratio=1e-4)
+getpy(fwdfine, zf, yfine, test)
 
 
+### some plots
+w2vpc <- w2vpcoarse[test,2]
+pyc <- pycoarse
+
+pdf("graphs/coarseprob.pdf", width=10, height=5)
+par(mfrow=c(1,2))
+hist(w2vpc[ycoarse[test]==0], col=rgb(1,0,0,.7), breaks=10, freq=FALSE,
+         xlab="prob(positive)", xlim=c(0,1), ylim=c(0,8), main="w2v inversion")
+hist(w2vpc[ycoarse[test]==1], col=rgb(1,1,0,.7), breaks=10, freq=FALSE, add=TRUE)
+
+hist(pyc[ycoarse[test]==0], col=rgb(1,0,0,.7), breaks=10, freq=FALSE,
+         xlab="prob(positive)", xlim=c(0,1), ylim=c(0,8), main="phrase regression")
+hist(pyc[ycoarse[test]==1], col=rgb(1,1,0,.7), breaks=10, freq=FALSE, add=TRUE)
+dev.off()
 
 
+pdf("graphs/coarseprob_bystar.pdf", width=10, height=5)
+par(mfrow=c(1,2))
+par(mfrow=c(1,2), mai=c(.7,.7,.5,.2), omi=c(.3,.3,0,0))
+boxplot( w2vpc ~ yfine[test], col=heat.colors(5), varwidth=TRUE, main="w2v inversion")
+boxplot( pyc~ yfine[test], col=heat.colors(5), varwidth=TRUE, main="phrase regression")
+mtext(side=1, "stars", outer=TRUE)
+mtext(side=2, "p(stars > 2)", outer=TRUE)
+dev.off()
 
+w2vpnnpy <- w2vpnnp[cbind(1:n,stars)]
+pdf("graphs/nnpprob.pdf", width=8, height=4)
+par(mfrow=c(1,2), mai=c(.7,.7,.5,.2), omi=c(.3,.3,0,0))
+boxplot( w2vpnnpy[test] ~ ynnp[test], col=heat.colors(5), varwidth=TRUE, main="w2v inversion")
+boxplot( pynnp~ ynnp[test], col=heat.colors(5), varwidth=TRUE, main="phrase regression")
+mtext(side=2, "p(y)", outer=TRUE)
+dev.off()
+
+w2vpy <- w2vprob[cbind(1:n,stars)]
+pdf("graphs/fineprob.pdf", width=8, height=4)
+par(mfrow=c(1,2), mai=c(.7,.7,.5,.2), omi=c(.3,.3,0,0))
+boxplot( w2vpy[test] ~ yfine[test], col=heat.colors(5), varwidth=TRUE, main="w2v inversion")
+points(1:5, nullprob, pch=18,cex=2, col="navy")
+boxplot( pyfine~ yfine[test], col=heat.colors(5), varwidth=TRUE, main="phrase regression")
+points(1:5, nullprob, pch=18,cex=2, col="navy")
+mtext(side=1, "stars", outer=TRUE)
+mtext(side=2, "p(y)", outer=TRUE)
+dev.off()
